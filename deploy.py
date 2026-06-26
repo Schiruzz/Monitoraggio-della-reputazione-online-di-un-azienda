d75dfff8866a1265dc5429e4a87e8d9a331d7f6c
Schiruzz <federicoschiratti@gmail.com>
2026-06-26 16:19:25 +0000

    deploy to Hf completed by adding requirements.txt to hf

--- deploy.py
+++ deploy.py
@@ -2,10 +2,24 @@ import os
 from huggingface_hub import HfApi
 
 api = HfApi()
+token = os.environ['HF_TOKEN']
+repo_id = 'Schiro/Monitoraggio_Della_Reputazione_Online'
+
+# Upload src files
 api.upload_folder(
     folder_path='src',
-    repo_id='Schiro/Monitoraggio_Della_Reputazione_Online',
+    repo_id=repo_id,
     repo_type='space',
-    token=os.environ['HF_TOKEN']
+    token=token
 )
+
+# Upload requirements.txt
+api.upload_file(
+    path_or_fileobj='requirements.txt',
+    path_in_repo='requirements.txt',
+    repo_id=repo_id,
+    repo_type='space',
+    token=token
+)
+
 print('Deploy completed!')
